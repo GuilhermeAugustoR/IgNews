fb5f6ca3b95377d04bd3d8472206f6bdcb581684
GuilhermeAugustoR <guilhermereiscampos23@gmail.com>
2022-07-25 10:59:54 -0300

    ✨ Criando o service da stripe e fazendo a requisicao

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,10 @@
 import styles from "./styles.module.scss";
 
-export function SubscribeButton() {
+interface SubscribeNuttonProps {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeNuttonProps) {
   return (
     <button type="button" className={styles.subscribeButton}>
       Subscribe now
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
+import { GetServerSideProps } from "next";
 import Head from "../../node_modules/next/head";
 import { SubscribeButton } from "../components/SubscribeButton/index";
+import { stripe } from "../services/strype";
 import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -17,9 +27,9 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding" />
@@ -27,3 +37,23 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1LPQhzJWeRbeN7a6qlbJPAI7", {
+    expand: ["product"],
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
