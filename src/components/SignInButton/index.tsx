c39f0b18f3f11e1b7abc9f7991bbd10fded8a28b
GuilhermeAugustoR <guilhermereiscampos23@gmail.com>
2022-08-09 15:24:07 -0300

    ✨ Login pelo github terminado

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,6 +1,6 @@
 import {FaGithub} from "react-icons/fa"
 import {FiX} from "react-icons/fi"
-import {signIn, useSession} from 'next-auth/react'
+import {signIn, useSession, signOut} from 'next-auth/react'
 import styles from "./styles.module.scss"
 
 export function SignInButton() {
@@ -10,9 +10,10 @@ export function SignInButton() {
         <button
             className={styles.signInButton}
             type="button"
+            onClick={() => signOut()}
         >
             <FaGithub color="#04d361"/>
-            Lucas Schiavini
+            {session.user.name}
             <FiX color="#737380" className={styles.closedIcon}/>
         </button>
     ) :
